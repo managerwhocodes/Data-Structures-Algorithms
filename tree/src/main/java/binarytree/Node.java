44e5e43c9f27d023c8f9807ca6d72385862ab514
managerwhocodes <managerwhocodes@gmail.com>
2020-04-11 09:20:52 +0530

    Tree Node

---
+++ tree/src/main/java/binarytree/Node.java
@@ -0,0 +1,45 @@
+package binarytree;
+
+public class Node{
+	private int value;
+	private int height;
+	private Node leftNode;
+	private Node rightNode;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Node getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+
+	public Node getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
+	
+	@Override
+	public String toString() {
+		return value + "";	
+	}
+}
