2c829a176425bf1316b75b5cf94bd131981df121
managerwhocodes <managerwhocodes@gmail.com>
2020-04-11 10:57:08 +0530

    Print leaf nodes

--- tree/src/main/java/binarytree/BinaryTree.java
+++ tree/src/main/java/binarytree/BinaryTree.java
@@ -1,7 +1,9 @@
 package binarytree;
 
 import java.util.Queue;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class BinaryTree{
 	
@@ -33,6 +35,23 @@ public class BinaryTree{
 		}	
 	}
 	
+	private List<Node> getLeafNodes(){
+		List<Node> list = new ArrayList<Node>();
+ 		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		while(!queue.isEmpty()){
+			Node currentNode = queue.poll();
+			if(currentNode.getLeftNode() == null && currentNode.getRightNode() == null)
+				list.add(currentNode);
+			if(currentNode.getLeftNode() !=null)
+				queue.add(currentNode.getLeftNode());
+			if(currentNode.getRightNode() !=null)
+				queue.add(currentNode.getRightNode());	
+				
+		}
+		return list;
+	}
+	
 	private void levelOrderTraversal() {
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.add(root);
@@ -55,5 +74,7 @@ public class BinaryTree{
 		}
 		
 		tree.levelOrderTraversal();
+		
+		System.out.println("Leaf Nodes : "+tree.getLeafNodes());
 	}
 }
