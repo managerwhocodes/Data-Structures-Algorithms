1a8f305fba2768253e5bae867fafd0313aed40af
managerwhocodes <managerwhocodes@gmail.com>
2020-04-14 09:35:31 +0530

    Find if the linked list is palindrome

---
+++ linkedlist/src/main/java/linkedlist/SingleLinkedListPalindrome.java
@@ -0,0 +1,41 @@
+package linkedlist;
+
+public class SingleLinkedListPalindrome extends SingleLinkedListReverse{
+
+    protected boolean isPalindrome(){
+    	
+		if (!existsLinkedList()) {
+            return false;
+        }
+
+    	// Divide the list in two equal parts
+    	Node second = divideList();
+    	
+    	// Reverse the second list 
+    	second = reverseLinkedList(second);
+    	
+    	Node first = head;
+    	
+    	while(first !=null && second !=null) {
+    		if(!compareNodes(first,second))
+    			return false;
+    		first = first.getNext();
+    		second = second.getNext();
+    	}    	
+    	return true;
+    }
+    
+    public static void main(String[] args){
+
+        SingleLinkedListPalindrome list = new SingleLinkedListPalindrome();
+        list.createLinkedList(new Node(1));
+        list.insertNodeAtEnd(new Node(2));
+        list.insertNodeAtEnd(new Node(3));
+        list.insertNodeAtEnd(new Node(3));
+        list.insertNodeAtEnd(new Node(2));
+        list.insertNodeAtEnd(new Node(1));
+
+        System.out.println(list.isPalindrome());
+
+    }
+}
