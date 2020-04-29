85c0603c6fb5f5a65c9d9cf1d35cb28208084aa6
managerwhocodes <managerwhocodes@gmail.com>
2020-04-29 23:23:53 +0530

    Print right side view of a binary tree

--- tree/src/main/java/binarytree/BinaryTreeViews.java
+++ tree/src/main/java/binarytree/BinaryTreeViews.java
@@ -32,6 +32,34 @@ public class BinaryTreeViews extends BinaryTree{
 			}
 		}
 	}
+	
+	protected void rightView(Node node) {
+
+		if(node == null)
+			return;
+		
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(node);
+				
+		while(!queue.isEmpty()){
+			int queueSize = queue.size();
+			int index = 0;
+			
+			while(index++ < queueSize) {
+				Node currentNode = queue.poll();
+				
+				if(index==queueSize) {
+					System.out.print(currentNode.getValue()+" ");
+				}
+				
+				if(currentNode.getLeftNode() != null)
+					queue.add(currentNode.getLeftNode());
+					
+				if(currentNode.getRightNode() != null)
+					queue.add(currentNode.getRightNode());							
+			}
+		}
+	}
 		
 	public static void main(String args[]){
 		BinaryTreeViews tree = new BinaryTreeViews();
@@ -42,6 +70,10 @@ public class BinaryTreeViews extends BinaryTree{
 			
 		System.out.print("Left view of the tree : ");
 		tree.leftView(tree.getRoot());
+		System.out.println("\n");
+		
+		System.out.print("Right view of the tree : ");
+		tree.rightView(tree.getRoot());
 		System.out.println("\n");
 		
 	}
