780c6990b36a2157cb74734df5b167ba95492c64
managerwhocodes <managerwhocodes@gmail.com>
2020-05-01 22:24:36 +0530

    Search a word in a trie

--- tree/src/main/java/trie/Trie.java
+++ tree/src/main/java/trie/Trie.java
@@ -8,7 +8,7 @@ public class Trie {
 		root = new Node();
 	}
 	
-	public void insert(String word) {
+	protected void insert(String word) {
 		Node currentNode = root;
 		for(int i=0; i < word.length(); i++) {
 			char ch = word.charAt(i);
@@ -21,10 +21,29 @@ public class Trie {
 		}
 		currentNode.end = true;
 	}
+	
+	protected boolean search(String word) {
+		Node currentNode = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			Node node = currentNode.children.get(ch); 
+			// word does not exists in trie;
+			if (node == null) {
+				return false;
+			}
+			currentNode = node;
+		}
+		// if currentNode.end == true then word exists in trie
+		// else it is a prefix of another word
+		return currentNode.end;
+	}
 
 	public static void main(String[] args) {
 		Trie trie = new Trie();
 		trie.insert("abcde");
+		
+		System.out.println("word abcde exists in trie : "+trie.search("abcde"));
+		System.out.println("word abc exists in trie : "+trie.search("abc"));
 		
 	}
 }
