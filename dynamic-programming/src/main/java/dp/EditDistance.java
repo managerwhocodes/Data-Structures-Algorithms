b200dc3c3a45d0be05edf7f9798a138a617e303d
managerwhocodes <managerwhocodes@gmail.com>
2020-05-08 22:35:59 +0530

    Edit Distance by Bottom-Up Approach

--- dynamic-programming/src/main/java/dp/EditDistance.java
+++ dynamic-programming/src/main/java/dp/EditDistance.java
@@ -32,6 +32,30 @@ public class EditDistance {
 		return dp[indexOne][indexTwo];
 	}
 	
+	// Bottom-Up Approach
+	protected int getMinOperationstoConvert_BU(String strOne, String strTwo) {
+		Integer [][]dp = new Integer[strOne.length()+1][strTwo.length()+1];
+		
+		for(int i = 0; i <= strOne.length(); i++) {
+			dp[i][0]  = i;
+		}
+		
+		for(int i = 0; i <= strTwo.length(); i++) {
+			dp[0][i] = i;
+		}
+		
+		for(int i = 1; i <= strOne.length(); i++) {
+			for(int j = 1; j <= strTwo.length(); j++) {
+				if(strOne.charAt(i-1) == strTwo.charAt(j-1)) {
+					dp[i][j] =  dp[i-1][j-1];
+				} else {
+					dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i][j-1], dp[i-1][j-1]));
+				}
+			}
+		}	
+		return dp[strOne.length()][strTwo.length()];	
+	}
+	
 	public static void main(String[] args) {
 		
 		EditDistance dis = new EditDistance();
@@ -42,7 +66,14 @@ public class EditDistance {
 							+inputStrTwo
 							+" to "
 							+inputStrOne
-							+" : "
-							+dis.getMinOperationstoConvert_TD(inputStrOne,inputStrTwo));	
+							+" using top down : "
+							+dis.getMinOperationstoConvert_TD(inputStrOne,inputStrTwo));
+		
+		System.out.print("\nMinimum number of operations to convert "
+				+inputStrTwo
+				+" to "
+				+inputStrOne
+				+" using bottom up : "
+				+dis.getMinOperationstoConvert_BU(inputStrOne,inputStrTwo));
 	}
 }
