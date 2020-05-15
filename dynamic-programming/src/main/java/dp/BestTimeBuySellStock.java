730191883a02772a570adbc9def7a36fe5414c10
managerwhocodes <managerwhocodes@gmail.com>
2020-05-15 22:48:59 +0530

    Best time to buy and sell stock with multiple transaction allowed

--- dynamic-programming/src/main/java/dp/BestTimeBuySellStock.java
+++ dynamic-programming/src/main/java/dp/BestTimeBuySellStock.java
@@ -2,7 +2,8 @@ package dp;
 
 public class BestTimeBuySellStock {
 	
-	private int maxProfit(int[] prices) {
+	// Allowed to complete at most one transaction 
+	private int maxProfitOneTrasaction(int[] prices) {
 
 		if(prices.length == 0) 
 			return 0;
@@ -18,14 +19,32 @@ public class BestTimeBuySellStock {
 		}
 		return profit[prices.length-1];  
     }
+	
+	// Allowed to complete multiple transactions
+	public int maxProfitMultipleTrasaction(int[] prices) {
+
+		if(prices.length == 0) 
+			return 0;
+
+		int profit = 0;
+
+		for(int i=1; i< prices.length;i++){
+			if(prices[i-1] < prices[i]) {
+				profit = profit + prices[i] - prices[i-1];
+			}		
+		}
+		return profit;
+    }
     
 	public static void main(String []args) {
 		BestTimeBuySellStock solution = new BestTimeBuySellStock();
 		int [] input = {7,1,5,3,6,4};
-		int output = solution.maxProfit(input);
+		int output = solution.maxProfitOneTrasaction(input);
 		int index = 1;
 		for(int price : input)
 			System.out.println("Price of stock on Day "+index+++" : "+price);
-		System.out.println("Maximum profit that can be made by buying and selling stocks : "+output);
+		System.out.println("Maximum profit that can be made by buying and selling stocks with one transaction allowed : "+output);
+		output = solution.maxProfitMultipleTrasaction(input);
+		System.out.println("Maximum profit that can be made by buying and selling stocks with multiple transaction allowed : "+output);
 	}
 }
