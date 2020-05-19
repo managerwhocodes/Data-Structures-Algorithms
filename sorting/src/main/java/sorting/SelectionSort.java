ee0855acb07865aacca6d198b159bc4d0f16d873
managerwhocodes <managerwhocodes@gmail.com>
2020-05-19 22:47:20 +0530

    Selection Sort

---
+++ sorting/src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,38 @@
+package sorting;
+
+public class SelectionSort {
+	
+	protected void selectionSort(int[] a) {
+		for (int j = 0; j<a.length; j++) {
+			int minimumIndex = j;
+			for (int i=j+1; i<a.length; i++) {
+				if (a[i] < a[minimumIndex])
+					minimumIndex = i;
+			}
+			if (minimumIndex != j) {
+				int temp = a[j];
+				a[j] = a[minimumIndex];
+				a[minimumIndex] = temp;
+			}
+		}
+	}
+	
+	protected static void printArray(int []array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+	}
+	
+	public static void main(String []args) {
+		int array[] = {10, 3, 2, 5, 8, 4, 3, 1, 2, 9, 7, 8};
+		SelectionSort ss = new SelectionSort();
+		
+		System.out.print("Before Sorting : ");
+		SelectionSort.printArray(array);
+		
+		ss.selectionSort(array);
+		
+		System.out.print("\nAfter sorting : ");
+		SelectionSort.printArray(array);
+	}
+}
