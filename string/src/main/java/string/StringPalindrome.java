a8fba76f457e433768d3e20e0374b37bc49f6cd4
managerwhocodes <managerwhocodes@gmail.com>
2020-05-25 19:43:55 +0530

    Check if chars of a string can be rearranged to form a palindrome

--- string/src/main/java/string/StringPalindrome.java
+++ string/src/main/java/string/StringPalindrome.java
@@ -1,27 +1,48 @@
 package string;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StringPalindrome {
 
 	
-	protected boolean isPalindrome(String str)
-	{
+	protected boolean isPalindrome(String str) {
+		
 		return isPalindromeUtil(str,0,str.length()-1);
 	}
 	
 	
-	protected boolean isPalindromeUtil(String str, int low, int high)
-	{
+	protected boolean isPalindromeUtil(String str, int low, int high) {
+		
 		return (low >= high) 
 				|| (str.charAt(low) == str.charAt(high) 
 				&& isPalindromeUtil(str, low + 1, high - 1));
 	}
 	
+	protected boolean canMakePalindrome(String str) {
+		
+		List<Character> list = new ArrayList<Character>(); 
+		
+		for (int i = 0; i < str.length(); i++) { 
+            if (list.contains(str.charAt(i))) 
+                list.remove((Character) str.charAt(i)); 
+            else
+                list.add(str.charAt(i)); 
+        } 
+		
+		if (str.length() % 2 == 0 && list.isEmpty() 
+				|| (str.length() % 2 == 1 && list.size() == 1)) 
+            return true; 
+        else
+            return false; 
+		
+	}
 	
-	protected boolean isAnagram(String strOne, String strTwo)
-	{
+	
+	protected boolean isAnagram(String strOne, String strTwo) {
+		
 		strOne = strOne.replaceAll(" ", "").toLowerCase();
 		strTwo = strTwo.replaceAll(" ", "").toLowerCase();
 		
@@ -47,11 +68,19 @@ public class StringPalindrome {
 		
 		StringPalindrome sp = new StringPalindrome();
 		String input = "ABCDDCBA";
-		System.out.println("Is string - "+input+" palindrome : "+sp.isPalindrome(input));
+		System.out.println("Is string - "+input
+							+" palindrome : "+sp.isPalindrome(input));
 		
 		String strOne = "Tom Marvolo Riddle";
 		String strTwo = "I Am Lord Voldemort";
 		
-		System.out.println("Are string - "+strOne+" and "+strTwo+" anagrams : "+sp.isAnagram(strOne,strTwo));
+		System.out.println("Are string - "+strOne
+							+" and "+strTwo
+							+" anagrams : "+sp.isAnagram(strOne,strTwo));
+		
+		String strFour = "geeksogeeks";
+		
+		System.out.println("Can the characters of the string - "+strFour
+							+" be rearranged to make a plaindrome : "+sp.canMakePalindrome(strFour));
 	}
 }
