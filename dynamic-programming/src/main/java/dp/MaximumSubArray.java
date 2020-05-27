8cda6670ec1044d9b5ce51c48654aa0c3b6f444c
managerwhocodes <managerwhocodes@gmail.com>
2020-05-27 23:41:47 +0530

    Maximum sum in a sub array

---
+++ dynamic-programming/src/main/java/dp/MaximumSubArray.java
@@ -0,0 +1,45 @@
+package dp;
+
+
+public class MaximumSubarray {
+	
+	// Kadane's Algorithm
+    protected int maxSubArray(int[] nums) {
+    	
+    	int sumMax = nums[0];
+    	int sumLocal = nums[0];
+    	
+    	for(int i=1;i<nums.length;i++) {
+    		sumLocal = Math.max(nums[i], sumLocal + nums[i]);
+    	    sumMax = Math.max(sumMax, sumLocal);
+    	}
+    	return sumMax;
+    }
+    	
+    // Dynamic Programming Approach	
+	protected int maxSubArrayDP(int[] nums) {	
+		
+		int [] solution = new int[nums.length+1];
+	    solution[0] = 0;
+	
+	    for (int j = 1; j <solution.length ; j++) {
+	        solution[j] = Math.max(solution[j-1]+ nums[j-1], nums[j-1]);
+	    }
+
+	    int result = solution[0];
+	    for (int j = 1; j <solution.length ; j++) {
+	        if(result<solution[j])
+	            result = solution[j];
+	    }
+	    return result;        
+    }
+    
+	public static void main(String []args) {
+		
+		MaximumSubarray ms = new MaximumSubarray();
+		int [] input = {-2,1,-3,4,-1,2,1,-5,4};
+		int output = ms.maxSubArray(input);
+		System.out.println(output);
+				
+	}
+}
