675d7ed98fce074a7a4f5d50076254ee1f67a44a
managerwhocodes <managerwhocodes@gmail.com>
2020-05-29 22:43:40 +0530

    Maximum product of sub array

---
+++ dynamic-programming/src/main/java/dp/MaximumProductSubarray.java
@@ -0,0 +1,25 @@
+package dp;
+
+public class MaximumProductSubarray {
+	
+    protected int maxProduct(int[] nums) {
+    	int prodMax = nums[0];
+    	int prodLocal = nums[0];
+    	
+    	for(int i=1;i<nums.length;i++) {
+    		prodLocal = Math.max(nums[i], prodLocal * nums[i]);
+    		prodMax = Math.max(prodMax, prodLocal);
+    	}
+    	
+    	return prodMax;
+        
+    }
+    
+	public static void main(String []args) {
+		MaximumProductSubarray mp = new MaximumProductSubarray();
+		int [] input = {-2,0,-1};
+		int output = mp.maxProduct(input);
+		System.out.println(" Maximum product of subarray : "+output);
+		
+	}
+}
