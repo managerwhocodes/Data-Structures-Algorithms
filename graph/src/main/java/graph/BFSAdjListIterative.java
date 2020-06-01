928fb06e3715cfc3cd704c370eef76331a0dd8f9
managerwhocodes <managerwhocodes@gmail.com>
2020-06-01 22:29:31 +0530

    Detect cycle in undirected graph using BFS

--- graph/src/main/java/graph/BFSAdjListIterative.java
+++ graph/src/main/java/graph/BFSAdjListIterative.java
@@ -31,14 +31,37 @@ public class BFSAdjListIterative {
 		}
 	}
 	
+	private boolean isCycle(GraphNode node) {
+		
+		LinkedList<GraphNode>queue = new LinkedList<GraphNode>();
+		queue.add(node);
+		
+		while(!queue.isEmpty()) {
+			GraphNode presentNode = queue.poll();
+			presentNode.setVisited(true);
+						
+			for(GraphNode neighbor: presentNode.getNeighbours()) { 
+				if(!neighbor.isVisited()) {
+					neighbor.setVisited(true);
+					queue.add(neighbor);
+					neighbor.setParent(presentNode);
+				} else if(presentNode.getParent()!= null && presentNode.getParent().getName() != neighbor.getName()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 	private void addUndirectedEdge(int i, int j) {
 		GraphNode source = nodeList.get(i);
 		GraphNode destination = nodeList.get(j);
 		source.getNeighbours().add(destination);
 		destination.getNeighbours().add(source);
 	}
-
+	
 	public static void main(String[] args) {
+		
 		
 		ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
 				
@@ -59,10 +82,36 @@ public class BFSAdjListIterative {
 		graph.addUndirectedEdge(5,8);
 		graph.addUndirectedEdge(6,7);
 		graph.addUndirectedEdge(7,8);
-		graph.addUndirectedEdge(8,9);		
+		graph.addUndirectedEdge(8,9);
 				
 		// Change this to start BFS from another source
-		System.out.println("Printing BFS from source: "+nodeList.get(0).getName());
+		System.out.println("Printing BFS from source : "+nodeList.get(0).getName());
 		graph.bfs(nodeList.get(0));
+		
+		
+		
+		ArrayList<GraphNode> nodeListTwo = new ArrayList<GraphNode>();
+		
+		for(int i=0;i<10; i++) {
+			nodeListTwo.add(new GraphNode("V"+i));
+		}
+		
+		BFSAdjListIterative graphTwo = new BFSAdjListIterative(nodeListTwo);
+		
+		graphTwo.addUndirectedEdge(0,1);
+		graphTwo.addUndirectedEdge(0,3);
+		graphTwo.addUndirectedEdge(1,2);
+		graphTwo.addUndirectedEdge(1,4);
+		graphTwo.addUndirectedEdge(2,5);
+		graphTwo.addUndirectedEdge(2,9);
+		graphTwo.addUndirectedEdge(3,6);
+		graphTwo.addUndirectedEdge(4,7);
+		graphTwo.addUndirectedEdge(5,8);
+		//graphTwo.addUndirectedEdge(6,7);
+		//graphTwo.addUndirectedEdge(7,8);
+		graphTwo.addUndirectedEdge(8,9);
+
+		System.out.println("\nIs there any cycle in the graph : "+graphTwo.isCycle(nodeListTwo.get(0)));
+
 	}	
 }
