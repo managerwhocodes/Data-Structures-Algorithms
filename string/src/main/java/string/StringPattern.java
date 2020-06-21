30e06381175f2812c0daecc619ed46d27e526da7
managerwhocodes <managerwhocodes@gmail.com>
2020-06-21 13:05:40 +0530

    Utility method for basic check

--- string/src/main/java/string/StringPattern.java
+++ string/src/main/java/string/StringPattern.java
@@ -1,40 +1,56 @@
 package string;
 
 public class StringPattern {
-
-	protected void find(String text, String pattern) {
-		int n = text.length();
-		int m = pattern.length();
-
-		int i = 0;
-		while (i <= n - m) {
-			for (int j = 0; j < m; j++) {
-				if (text.charAt(i + j) != pattern.charAt(j))
-					break;
-
-				if (j == m - 1)
-					System.out.println("Pattern occurs with shift " + i);
-			}
-			i++;
-		}
-	}
 	
-	protected void KMP(String X, String Y) {
-
-		if (Y == null || Y.length() == 0) {
+	
+	private void basicCheck(String text, String pattern) {
+		
+		if (pattern == null || pattern.length() == 0) {
 			System.out.println("Pattern occurs with shift 0");
 			return;
 		}
 
-		if (X == null || Y.length() > X.length()) {
+		if (text == null || pattern.length() > text.length()) {
 			System.out.println("Pattern not found");
 			return;
 		}
+	}
 
-		char[] chars = Y.toCharArray();
+	// Basic patter matching
+	// Time Complexity for this approach is O(mn) 
+	// where n is length of string and m is length of pattern
+	protected void basicPatternMatching(String text, String pattern) {
+		
+		basicCheck(text,pattern);
+		
+		int textLength = text.length();
+		int patternLength = pattern.length();
 
-		int[] next = new int[Y.length() + 1];
-		for (int i = 1; i < Y.length(); i++) {
+		int i = 0;
+		long startTime = System.nanoTime();
+		while (i <= textLength - patternLength) {
+			for (int j = 0; j < patternLength; j++) {
+				if (text.charAt(i + j) != pattern.charAt(j))
+					break;
+
+				if (j == patternLength - 1)
+					System.out.println("Pattern occurs with shift " + i);
+			}
+			i++;
+		}
+		long endTime = System.nanoTime();
+		System.out.print("Total time taken in nanosecs : "+(endTime-startTime));
+	}
+	
+	protected void KMP(String text, String pattern) {
+
+		basicCheck(text,pattern);
+
+		char[] chars = pattern.toCharArray();
+		int[] next = new int[pattern.length() + 1];
+		
+		long startTime = System.nanoTime();
+		for (int i = 1; i < pattern.length(); i++) {
 			int j = next[i + 1];
 
 			while (j > 0 && chars[j] != chars[i])
@@ -44,9 +60,9 @@ public class StringPattern {
 				next[i + 1] = j + 1;
 		}
 
-		for (int i = 0, j = 0; i < X.length(); i++) {
-			if (j < Y.length() && X.charAt(i) == Y.charAt(j)) {
-				if (++j == Y.length()) {
+		for (int i = 0, j = 0; i < text.length(); i++) {
+			if (j < pattern.length() && text.charAt(i) == pattern.charAt(j)) {
+				if (++j == pattern.length()) {
 					System.out.println("Pattern occurs with shift " +
 									(i - j + 1));
 				}
@@ -56,16 +72,30 @@ public class StringPattern {
 				i--;
 			}
 		}
+		long endTime = System.nanoTime();
+		System.out.print("Total time taken in nanosecs : "+(endTime-startTime));
 	}
 	
 	public static void main(String[] args) {
 		
 		StringPattern sp = new StringPattern();
 		
-		String text = "ABCABAABCABAC";
-		String pattern = "CAB";
+		//String text = "ABCABAABCABAC";
+		//String pattern = "CAB";
+		
+		String text = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		String pattern = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB";
 
-		sp.find(text, pattern);
+		
+		System.out.print("Input text : "+text);
+		System.out.print("\nInput pattern : "+pattern);
+		
+		System.out.println("\n\nResult using basic pattern finding method : ");		
+		sp.basicPatternMatching(text, pattern);
+		
+		System.out.println("\n\nResult using Knuth-Morris-Pratt Algorithm : ");		
 		sp.KMP(text, pattern);
+		
+		//sp.KMP(text, pattern);
 	}
 }
