8d42bc25de6aa57a9d6df241ca0ffe7ef37a2d0f
managerwhocodes <managerwhocodes@gmail.com>
2020-06-25 21:24:34 +0530

    Bottom up approach

--- dynamic-programming/src/main/java/dp/MinimumJumps.java
+++ dynamic-programming/src/main/java/dp/MinimumJumps.java
@@ -1,9 +1,11 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MinimumJumps {
 
 	// Top-Down Approach
-	protected int findMinJumps(int[] arr, int i, int n, int lookup[]) {
+	protected int findMinJumpsTopDown(int[] arr, int i, int n, int lookup[]) {
 
 		if (i == n - 1) {
 			return 0;
@@ -20,13 +22,35 @@ public class MinimumJumps {
 		int min_jumps = Integer.MAX_VALUE;
 		
 		for (int j = i + 1; j <= i + arr[i]; j++) {
-			int cost = findMinJumps(arr, j, n, lookup);
+			int cost = findMinJumpsTopDown(arr, j, n, lookup);
 			if (cost != Integer.MAX_VALUE) {
 				min_jumps = Math.min(min_jumps, cost + 1);
 			}
 		}
-
 		return (lookup[i] = min_jumps);
+	}
+	
+	// Bottom-Up Approach
+	protected int findMinJumpsBottomUp(int[] arr) {
+
+		int n = arr.length;
+		
+		if (arr[0] == 0) {
+			return Integer.MAX_VALUE;
+		}
+
+		int[] lookup = new int[n];
+		Arrays.fill(lookup, Integer.MAX_VALUE);
+
+		lookup[0] = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; (i + j < n) && j <= Math.min(n - 1, arr[i]); j++) {
+				lookup[i + j] = (lookup[i + j] != Integer.MAX_VALUE) ?
+								Math.min(lookup[i + j], lookup[i] + 1): (lookup[i] + 1);
+			}
+		}
+		return lookup[n - 1];
 	}
 
 	public static void main(String[] args) {
@@ -36,9 +60,11 @@ public class MinimumJumps {
 		int[] arr = { 1, 3, 6, 1, 0, 9 };
 		int[] lookup = new int[arr.length];
 
-		System.out.println("Minimum jumps required to reach the destination are " +
-				jumps.findMinJumps(arr, 0, arr.length, lookup));
+		System.out.println("Minimum jumps required to reach the destination using top-down approach : " +
+				jumps.findMinJumpsTopDown(arr, 0, arr.length, lookup));
+		
+		System.out.println("Minimum jumps required to reach the destination using bottom-up approach : " +
+				jumps.findMinJumpsBottomUp(arr));
 
 	}
-
 }
