8bdf8b2e9b75f3dc45a45eeca00d38cbc75d8253
managerwhocodes <managerwhocodes@gmail.com>
2020-06-29 23:37:54 +0530

    Top-Down approach

--- dynamic-programming/src/main/java/dp/GoldPots.java
+++ dynamic-programming/src/main/java/dp/GoldPots.java
@@ -20,6 +20,29 @@ public class GoldPots {
 		
 		return Integer.max(start, end);
 	}
+	
+	protected int optimalStrategyTopDown(int[] coin, int i, int j, int[][] lookup) {
+
+		if (i == j) {
+			return coin[i];
+		}
+	
+		if (i + 1 == j) {
+			return Integer.max(coin[i], coin[j]);
+		}
+
+		if (lookup[i][j] == 0) {
+			
+			int start = coin[i] + Integer.min(optimalStrategyTopDown(coin, i + 2, j, lookup),
+												optimalStrategyTopDown(coin, i + 1, j - 1, lookup));
+
+			int end = coin[j] + Integer.min(optimalStrategyTopDown(coin, i + 1, j - 1, lookup), 
+											optimalStrategyTopDown(coin, i, j - 2, lookup));
+	
+			lookup[i][j] = Integer.max(start, end);
+		}
+		return lookup[i][j];
+	}
 
 	public static void main(String[] args) {
 		
@@ -27,9 +50,13 @@ public class GoldPots {
 		
 		int[] coin = { 4, 6, 2, 3 };
 
-		System.out.print("Maximum coins collected by player is "
+		System.out.println("Maximum coins collected by player is "
 						+ gp.optimalStrategy(coin, 0, coin.length - 1));
+		
+		int[][] lookup = new int[coin.length][coin.length];
+		
+		System.out.println("Maximum coins collected by player is "
+				+ gp.optimalStrategyTopDown(coin, 0, coin.length - 1, lookup));
 
 	}
-
 }
