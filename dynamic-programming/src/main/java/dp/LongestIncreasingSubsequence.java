200f2debb3031a1714b087c7d929064a135044b4
managerwhocodes <managerwhocodes@gmail.com>
2020-07-14 10:32:39 +0530

    Longest increasing subsequence using top down approach

--- dynamic-programming/src/main/java/dp/LongestIncreasingSubsequence.java
+++ dynamic-programming/src/main/java/dp/LongestIncreasingSubsequence.java
@@ -21,6 +21,28 @@ public class LongestIncreasingSubsequence {
 		return Math.max(includeCount, excludeCount);	
 	}
 	
+	// Top-down
+	protected int findLIS_TD(int dp[], int arr[], int i, int prev) {
+		
+		// Base condition
+		if(i >= arr.length)
+			return 0;
+		
+		if(dp[i] != 0)
+			return dp[i];
+		
+		int includeCount = 0;
+		if(arr[i] > prev) {
+			includeCount = 1 + findLIS(arr, i+1, arr[i]);
+			dp[i] = includeCount;
+		}
+		
+		int excludeCount = findLIS(arr, i+1, prev);
+		dp[i] = Math.max(includeCount, excludeCount);
+		
+		return dp[i];	
+	}	
+	
 	public static void main(String[] args) {
 		
 		LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
@@ -29,8 +51,14 @@ public class LongestIncreasingSubsequence {
 
 		System.out.print("Input Array : "+Arrays.toString(input));
 		
-		System.out.print("\nLength of LIS : "
+		System.out.print("\nLength of LIS using recursion : "
 						+ lis.findLIS(input, 0, Integer.MIN_VALUE));
+		
+		
+		int []dp = new int[input.length];
+		
+		System.out.print("\nLength of LIS using top down : "
+				+ lis.findLIS_TD(dp,input, 0, Integer.MIN_VALUE));
 
 	}
 }
