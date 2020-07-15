f96c726cb7cce020ebf76e784c0c6e8e32356fbf
managerwhocodes <managerwhocodes@gmail.com>
2020-07-15 08:30:36 +0530

    Using top down approach

--- dynamic-programming/src/main/java/dp/MaximumSumIncreasingSubsequence.java
+++ dynamic-programming/src/main/java/dp/MaximumSumIncreasingSubsequence.java
@@ -21,6 +21,27 @@ public class MaximumSumIncreasingSubsequence {
 		return Math.max(includeCurrentSum, excludeCurrentSum);	
 	}
 	
+	// Top Down Approach
+	protected int findLIS_TD(int dp[] , int arr[], int i, int prev, int sum) {
+		
+		// Base condition
+		if(i >= arr.length)
+			return sum;
+		
+		if(dp[i] != 0)
+			return dp[i];
+		
+		int includeCurrentSum = sum;
+		if(arr[i] > prev) {
+			includeCurrentSum = findLIS(arr, i+1, arr[i], sum+arr[i]);
+		}
+		
+		int excludeCurrentSum = findLIS(arr, i+1, prev,sum);
+		dp[i] = Math.max(includeCurrentSum, excludeCurrentSum);
+		
+		return dp[i];	
+	}
+	
 
 	public static void main(String[] args) {
 		
@@ -32,6 +53,11 @@ public class MaximumSumIncreasingSubsequence {
 		
 		System.out.print("\nMaximum sum of increasing subsequence using recursion : "
 						+ msis.findLIS(input, 0, Integer.MIN_VALUE, 0));
+		
+		int dp[] = new int[input.length];
+		
+		System.out.print("\nMaximum sum of increasing subsequence using recursion : "
+				+ msis.findLIS_TD(dp, input, 0, Integer.MIN_VALUE, 0));
 		
 	}
 }
