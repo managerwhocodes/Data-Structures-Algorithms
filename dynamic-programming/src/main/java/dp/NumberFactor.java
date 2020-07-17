f296160079f17bce1ff25d3f477a16ba6066e350
managerwhocodes <managerwhocodes@gmail.com>
2020-07-17 23:08:26 +0530

    Using Recursion

--- dynamic-programming/src/main/java/dp/NumberFactor.java
+++ dynamic-programming/src/main/java/dp/NumberFactor.java
@@ -2,6 +2,25 @@ package dp;
 
 public class NumberFactor {
 	
+	// Recursive
+	protected int waystoGetN(int num) {
+		
+		// Base conditions
+		if(num == 0 || num == 1 || num == 2)
+			return 1;
+		
+		if(num == 3)
+			return 2;
+		
+		int waystoGetSelecting1 = waystoGetN(num-1);
+		int waystoGetSelecting3 = waystoGetN(num-3);
+		int waystoGetSelecting4 = waystoGetN(num-4);
+		
+		return (waystoGetSelecting1 + waystoGetSelecting3 + waystoGetSelecting4);
+		
+	}
+	
+	
 	// Top-Down Approach
 	protected int waysToGetN_TD(int num) {
 		int dp[] = new int[num+1];
@@ -39,7 +58,11 @@ public class NumberFactor {
 		int input = 6;
 		
 		System.out.println("Numbers of ways to express "+input
+				+" as a sum of 1,3,4 using Top Down Approach : "+ nc.waystoGetN(input));
+		
+		System.out.println("Numbers of ways to express "+input
 							+" as a sum of 1,3,4 using Top Down Approach : "+ nc.waysToGetN_TD(input));
+		
 		System.out.println("Numbers of ways to express "+input
 							+" as a sum of 1,3,4 using Bottom Up Approach : "+ nc.waysToGetN_BU(input));
 	}
