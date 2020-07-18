c64abf777d0f6677a86a493272a0f7102a601b72
managerwhocodes <managerwhocodes@gmail.com>
2020-07-18 08:49:07 +0530

    Use top down approach

--- dynamic-programming/src/main/java/dp/LongestDecreasingSubsequence.java
+++ dynamic-programming/src/main/java/dp/LongestDecreasingSubsequence.java
@@ -19,6 +19,27 @@ public class LongestDecreasingSubsequence {
 		int excludeCount = findDIS(arr, i+1, prev);
 		
 		return Math.max(includeCount, excludeCount);	
+	}
+	
+	// Top-down
+	protected int findDIS_TD(Integer dp[][], int arr[], int curr, int prev) {
+		
+		// Base condition
+		if(curr >= arr.length)
+			return 0;
+		
+		if(dp[prev+1][curr] != null)
+			return dp[prev+1][curr];
+		
+		int includeCount = 0;
+		if(prev < 0 || arr[curr] < arr[prev]) {
+			includeCount = 1 + findDIS_TD(dp, arr, curr+1, curr);
+		}
+		
+		int excludeCount = findDIS_TD(dp, arr, curr+1, prev);
+		dp[prev+1][curr] = Math.max(includeCount, excludeCount);
+		
+		return dp[prev+1][curr];	
 	}	
 	
 	public static void main(String[] args) {
@@ -31,6 +52,12 @@ public class LongestDecreasingSubsequence {
 		
 		System.out.print("\nLength of LIS using recursion : "
 						+ lis.findDIS(input, 0, Integer.MAX_VALUE));
+		
+		
+		Integer [][]dp = new Integer[input.length+1][input.length];
+		
+		System.out.print("\nLength of LIS using top down : "
+				+ lis.findDIS_TD(dp,input, 0, -1));
 
 	}
 }
