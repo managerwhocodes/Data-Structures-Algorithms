3699387c4fb582cdfa4918db5457cb276be80fda
managerwhocodes <managerwhocodes@gmail.com>
2020-07-19 20:05:41 +0530

    Use recursion

--- dynamic-programming/src/main/java/dp/ClimbStairs.java
+++ dynamic-programming/src/main/java/dp/ClimbStairs.java
@@ -1,7 +1,28 @@
 package dp;
 
 public class ClimbStairs {
+	
+	// Using Recursion
+    public int minCostClimbingStairs(int [] cost, int i,int min) {
+    	
+    	if(i < 0  || cost.length<=1)
+    		return 0;
+    	
+    	if(i >= cost.length)
+    		return 0;
+
+    	if(i+1 == cost.length)
+    		return min;
+ 	
+    	if(cost[i]<cost[i+1])  	
+   	   		min = cost[i]  + minCostClimbingStairs(cost ,i+1,min);  	   	
+    	else
+    		min = cost[i+1] + minCostClimbingStairs(cost ,i+2,min);
+   	
+		return min;
+    }
 	 
+    // Using Top Down - space optimised
     public int minCostClimbingStairs(int [] cost) {
     	
    	   	int stepZero = 0;
@@ -18,8 +39,10 @@ public class ClimbStairs {
     }
 
 	public static void main(String[] args) {
-		int [] cost  = {100 , 15, 30, 25, 5, 30};
+		int [] cost  = {100 , 15, 30, 25, 5, 30, 2, 90, 6, 1, 1 ,1};
 		ClimbStairs sol = new ClimbStairs();
-		System.out.println(sol.minCostClimbingStairs(cost));		
+		System.out.println(sol.minCostClimbingStairs(cost));	
+		
+		System.out.println(sol.minCostClimbingStairs(cost,0,0));
 	}
 }
