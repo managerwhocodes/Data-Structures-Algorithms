a89c19304eeba2593b24cc2902c438f389a855c4
managerwhocodes <managerwhocodes@gmail.com>
2020-08-01 18:11:15 +0530

    Word break problem

--- tree/src/main/java/trie/Dictionary.java
+++ tree/src/main/java/trie/Dictionary.java
@@ -24,6 +24,33 @@ public class Dictionary extends Trie{
 			}
 		}
     }
+    
+    private boolean wordBreak(String str) {
+    	boolean isBreak = true;
+    	Node currentNode = root;
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			Node node = currentNode.children.get(ch); 
+
+			if (node == null) {
+				isBreak = false;
+				return isBreak;
+			} 
+						
+			currentNode = node;
+			
+			if(currentNode.wordEnd == true) {
+				System.out.println(str.substring(0, i+1)+" ");
+				str = str.substring(i+1, str.length());
+				currentNode = root;
+				i=-1;
+			}
+			
+		}
+		// if currentNode.end == true then word exists in trie
+		// else it is a prefix of another word
+		return isBreak;
+    }
 
 	public static void main(String[] args) {
 
@@ -44,5 +71,17 @@ public class Dictionary extends Trie{
 		System.out.println("Lexicographic Order : ");
 		StringBuilder sb = new StringBuilder();
 		dic.preOrderTraversal(dic.getRoot(), sb);
+
+		Dictionary dic2 = new Dictionary();
+		
+		List<String> dictionary2 = Arrays.asList("this", "th", "is", "famous",
+				"word", "break", "b", "r", "e", "a", "k",
+				"br", "bre", "brea", "ak", "problem");
+		
+		for( String word : dictionary2) {
+			dic2.insert(word);
+		}
+		String str = "wordbreakproblem";
+		dic2.wordBreak(str);
 	}
 }
