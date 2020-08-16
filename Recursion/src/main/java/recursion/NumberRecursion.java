73e0a05b9ffa7e796f27667be476531ebc4a3b73
managerwhocodes <managerwhocodes@gmail.com>
2020-08-16 12:12:37 +0530

    Calculate modulus

--- Recursion/src/main/java/recursion/NumberRecursion.java
+++ Recursion/src/main/java/recursion/NumberRecursion.java
@@ -2,7 +2,7 @@ package recursion;
 
 public class NumberRecursion {
 
-	private int getPower(int number, int power){
+	protected int getPower(int number, int power){
         
         if(power==1)
             return number;
@@ -11,12 +11,28 @@ public class NumberRecursion {
 
     }
 	
+    protected int getMod(int dividend, int divisor){
+        
+        if(divisor == 0){
+            System.out.println("Divisor cannot be 0");
+            return -1;
+        }
+
+        if(dividend < divisor){
+            return dividend;
+        }
+        
+        return getMod(dividend - divisor, divisor);
+        
+    }
+	
 	public static void main(String[] args) {
 		
 		NumberRecursion recursion = new NumberRecursion(); 
 		
-		System.out.println("4 power 3 = "+recursion.getPower(4,3));
+		System.out.println(" 4  power 3 = "+recursion.getPower(4,3));
+		
+		System.out.println(" 17  mod  3 = "+recursion.getMod(17,3));
 
 	}
-
 }
