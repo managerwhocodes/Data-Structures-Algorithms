d70d7e1b234546a974096636043dc428157931e1
managerwhocodes <managerwhocodes@gmail.com>
2020-08-17 21:26:15 +0530

    Remove adjacent duplicate characters

--- Recursion/src/main/java/recursion/StringRecursion.java
+++ Recursion/src/main/java/recursion/StringRecursion.java
@@ -13,12 +13,29 @@ public class StringRecursion {
         //return string.charAt(string.length()-1) + reverseString(string.substring(0,string.length()-1));  
     }
 	
+	protected String removeDuplicates(String text) {
+        
+        if (text.length() == 1) {
+            return text;
+        }
+    
+        if (text.substring(0,1).equals(text.substring(1,2))) {
+            return removeDuplicates(text.substring(1));
+        }
+        else {
+            return text.substring(0,1) + removeDuplicates(text.substring(1));
+        }
+    }
+	
 	public static void main(String[] args) {
 		
 		StringRecursion strRec = new StringRecursion();
         
 		String inputStr = "This is test";
         System.out.println("The reverse of "+inputStr+" = "+strRec.reverseString(inputStr));
+        
+        inputStr = "Thiss iiss aa teesstt";
+        System.out.println("Remove duplicates from '"+inputStr+"' = "+strRec.removeDuplicates(inputStr));
 
 	}
 
