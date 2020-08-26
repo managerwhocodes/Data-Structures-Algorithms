7896b92f6085beaea14ad10b19935fff956ee2a9
managerwhocodes <managerwhocodes@gmail.com>
2020-08-27 04:07:15 +0530

    Find the length of the longest substring having the same letters after replacement

--- array/src/main/java/array/SlidingWindow.java
+++ array/src/main/java/array/SlidingWindow.java
@@ -109,6 +109,31 @@ public class SlidingWindow {
 		}
 		return maxLength;		
 	}
+	
+	protected int findLenOfLongestSubstringAfterReplacement(String str, int k) {
+	    if (str == null || str.length() == 0)
+		      throw new IllegalArgumentException("Invalid Input");
+		
+		int windowStart = 0, maxLength = 0, maxRepeatLetterCount = 0;
+		Map<Character, Integer> letterFrequencyMap = new HashMap<Character, Integer>();
+		
+		for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+			char rightChar = str.charAt(windowEnd);
+		    letterFrequencyMap.put(rightChar, letterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+		    maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
+		    
+		    
+		    if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+		    	 char leftChar = str.charAt(windowStart);
+		         letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+		         windowStart++;
+		    }
+		    
+		    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+		}
+		
+		return maxLength;
+	}
 
 	public static void main(String[] args) {
 		
@@ -131,6 +156,8 @@ public class SlidingWindow {
 		System.out.println("Length of the longest substring: " + sw.findLenOfLongestSubStrWithKChar("cbbebi", 3));
 		
 		System.out.println("Length of the longest substring: " + sw.findLenOfLongestSubstringWithoutRep("aabccbb"));
+		
+		System.out.println(sw.findLenOfLongestSubstringAfterReplacement("aabccbb", 2));
 		
 	}
 
