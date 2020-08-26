c35182d667fe0a0c4349966751f007ffe8d1e5f9
managerwhocodes <managerwhocodes@gmail.com>
2020-08-26 19:22:01 +0530

    find the maximum sum of any contiguous sub-array of size ‘k’.

--- array/src/main/java/array/SlidingWindow.java
+++ array/src/main/java/array/SlidingWindow.java
@@ -24,6 +24,24 @@ public class SlidingWindow {
 		}
 		return result;				
 	}
+	
+	
+	protected int findMaxSumSubArray(int[] input, int k) {
+	    
+		int maxSum = Integer.MIN_VALUE;
+		int windowSum = 0;
+		int windowStart = 0;
+		
+		for(int windowEnd = 0; windowEnd < input.length; windowEnd++) {
+			windowSum += input[windowEnd];
+			if(windowEnd >= k-1) {
+				maxSum = Math.max(maxSum, windowSum);
+				windowSum -= input[windowStart];
+				windowStart++;
+			}
+		}
+	    return maxSum;
+	  }
 
 	public static void main(String[] args) {
 		
@@ -32,7 +50,11 @@ public class SlidingWindow {
 		int []input = {1, 3, 2, 6, -1, 4, 1, 8, 2};
 		int k = 5;
 		int []result = sw.findAvgOfSubArraySizeK(input, k);
-		System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+		System.out.println("Averages of subarrays of size K: " 
+				+ Arrays.toString(result));
+		
+		System.out.println("Maximum sum of a subarray of size K: "
+		        + sw.findMaxSumSubArray(input , k));
 	}
 
 }
