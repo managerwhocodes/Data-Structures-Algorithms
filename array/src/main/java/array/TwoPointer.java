38265377c7ab09d14602c59f1b1e1e1cd1bed5ba
managerwhocodes <managerwhocodes@gmail.com>
2020-08-29 12:10:35 +0530

    Remove duplicates and remove all occurrences

--- array/src/main/java/array/TwoPointer.java
+++ array/src/main/java/array/TwoPointer.java
@@ -1,6 +1,5 @@
 package array;
 
-
 public class TwoPointer {
 
 	protected int[] findPairWithTargetSum(int input[], int targetSum) {
@@ -24,12 +23,46 @@ public class TwoPointer {
 		}
 		return new int[] {-1,-1};
 	}
+	
+	protected int removeDuplicates(int []input) {
+		
+		int nextNonDuplicate = 1;
+		
+		for(int i=1; i<input.length; i++) {
+			if (input[nextNonDuplicate-1] != input[i]) {
+				input[nextNonDuplicate] = input[i];
+				nextNonDuplicate++;
+	    	}
+		}
+
+		return nextNonDuplicate;
+	}
+	
+	
+	protected int removeAllOccurencesOfK(int []input , int k) {
+		
+		int nextElement = 0;
+		
+		for(int i=0; i<input.length; i++) {
+			if (input[i] != k) {
+				input[nextElement] = input[i];
+				nextElement++;
+	    	}
+		}
+			
+		return nextElement;
+	}
 
 	public static void main(String[] args) {
 		TwoPointer tp = new TwoPointer();
 		
-		int[] result = tp.findPairWithTargetSum(new int[] { 1, 2, 3, 4, 6 }, 6);
+		int []input = { 1, 2, 3, 4, 6 };
+		
+		int[] result = tp.findPairWithTargetSum(input, 6);
 		System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+		
+	    System.out.println(tp.removeDuplicates(new int[] { 2, 2, 2, 11 }));
+	    System.out.println(tp.removeAllOccurencesOfK(new int[] { 2, 11, 2, 2, 1}, 2));
 	}
 
 }
