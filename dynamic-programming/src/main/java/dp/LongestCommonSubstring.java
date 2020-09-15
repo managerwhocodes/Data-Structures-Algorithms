0ac604ca979081fdc80967de82490a21c279985d
managerwhocodes <managerwhocodes@gmail.com>
2020-09-15 23:27:02 +0530

    Minimum number of elements that should be deleted to make the remaining sequence sorted.

--- dynamic-programming/src/main/java/dp/LongestCommonSubstring.java
+++ dynamic-programming/src/main/java/dp/LongestCommonSubstring.java
@@ -54,6 +54,26 @@ public class LongestCommonSubstring {
 		return dp[indexOne][indexTwo][count];			
 	}
 	
+	protected int findMinimumDeletions(int[] nums){
+		return nums.length - findLISLength(nums);
+	}
+
+	private int findLISLength(int[] nums) {
+		int[] dp = new int[nums.length];
+		dp[0] = 1;
+		
+		int maxLength = 1;
+		for (int i=1; i<nums.length; i++) {
+		  dp[i] = 1;
+		  for (int j=0; j<i; j++)
+		    if (nums[i] > nums[j] && dp[i] <= dp[j] ) {
+		      dp[i] = dp[j]+1;
+		      maxLength = Math.max(maxLength, dp[i]);
+		    }
+		}
+		return maxLength;
+	 }
+	
 	public static void main(String[] args) {
 		
 		LongestCommonSubstring lcs = new LongestCommonSubstring();
