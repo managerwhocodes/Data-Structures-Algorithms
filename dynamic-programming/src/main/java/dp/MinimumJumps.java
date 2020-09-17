21a373c8337e17982f8d6919f9c26ce646e5f225
managerwhocodes <managerwhocodes@gmail.com>
2020-09-17 21:14:09 +0530

    Minimum fee required to reach the top of the staircase

--- dynamic-programming/src/main/java/dp/MinimumJumps.java
+++ dynamic-programming/src/main/java/dp/MinimumJumps.java
@@ -83,6 +83,20 @@ public class MinimumJumps {
 		}
 		return lookup[arrLength - 1];
 	}
+	
+	
+	protected int findMinFee(int[] fee) {
+	    int dp[] = new int[fee.length + 1]; 
+	    dp[0] = 0; 
+	    dp[1] = fee[0]; 
+	    dp[2] = dp[3] = fee[0];
+
+	    for (int i = 3; i < fee.length; i++)
+	      dp[i + 1] = Math.min(fee[i] + dp[i], Math.min(fee[i - 1] + dp[i - 1], fee[i - 2] + dp[i - 2]));
+
+	    return dp[fee.length];
+	  }
+
 
 	public static void main(String[] args) {
 		
@@ -133,6 +147,12 @@ public class MinimumJumps {
 		else
 			System.out.println("Minimum jumps required to reach the destination using bottom-up approach : " +
 				jumps.findMinJumpsBottomUp(inputArrTwo));
+		
+		
+		int[] fee = { 1, 2, 5, 2, 1, 2 };
+	    System.out.println(jumps.findMinFee(fee));
+	    fee = new int[] { 2, 3, 4, 5 };
+	    System.out.println(jumps.findMinFee(fee));
 
 	}
 }
