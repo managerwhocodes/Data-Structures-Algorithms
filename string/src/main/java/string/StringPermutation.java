9555e4fbd8cd8d17057601d90af6d7f88e3e919a
managerwhocodes <managerwhocodes@gmail.com>
2020-09-19 21:01:35 +0530

    Find all distinct subsets

--- string/src/main/java/string/StringPermutation.java
+++ string/src/main/java/string/StringPermutation.java
@@ -186,22 +186,44 @@ public class StringPermutation {
         permutations.add(str);
 
         for (int i = 0; i < str.length(); i++) {
-          if (Character.isLetter(str.charAt(i))) {
+        	if (Character.isLetter(str.charAt(i))) {
 
-            int n = permutations.size();
-            for (int j = 0; j < n; j++) {
-              char[] chs = permutations.get(j).toCharArray();
+        		int n = permutations.size();
+        		for (int j = 0; j < n; j++) {
+        			char[] chs = permutations.get(j).toCharArray();
 
-              if (Character.isUpperCase(chs[i]))
-                chs[i] = Character.toLowerCase(chs[i]);
-              else
-                chs[i] = Character.toUpperCase(chs[i]);
-              permutations.add(String.valueOf(chs));
-            }
-          }
+        			if (Character.isUpperCase(chs[i]))
+        				chs[i] = Character.toLowerCase(chs[i]);
+        			else
+        				chs[i] = Character.toUpperCase(chs[i]);
+        			permutations.add(String.valueOf(chs));
+        		}
+        	}
         }
         return permutations;
-      }
+    }
+    
+    
+    protected List<List<Integer>> findSubsets(int[] nums) {
+
+    	Arrays.sort(nums);
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());
+        int startIndex = 0, endIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+        	startIndex = 0;
+
+        	if (i > 0 && nums[i] == nums[i - 1])
+        		startIndex = endIndex + 1;
+        	endIndex = subsets.size() - 1;
+        	for (int j = startIndex; j <= endIndex; j++) {
+        		List<Integer> set = new ArrayList<>(subsets.get(j));
+        		set.add(nums[i]);
+        		subsets.add(set);
+        	}
+        }
+        return subsets;
+    }
     
     public static void main(String args[]) {
     	
