befd12f8aef39323a66b603de4f241111b749131
managerwhocodes <managerwhocodes@gmail.com>
2020-09-22 18:47:37 +0530

    Given an unsorted array of numbers, find Kth smallest number in it.

--- array/src/main/java/array/KNumbers.java
+++ array/src/main/java/array/KNumbers.java
@@ -22,6 +22,22 @@ public class KNumbers {
 		return new ArrayList<>(minHeap);
 	}
 	
+	protected int findKthSmallestNumber(int[] nums, int k) {
+	    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((n1, n2) -> n2 - n1);
+
+	    for (int i = 0; i < k; i++)
+	      maxHeap.add(nums[i]);
+
+	    for (int i = k; i < nums.length; i++) {
+	    	if (nums[i] < maxHeap.peek()) {
+	    		maxHeap.poll();
+	    		maxHeap.add(nums[i]);
+	    	}
+	    }
+
+	    return maxHeap.peek();
+	 }
+	
 	public static void main(String[] args) {
 		
 		KNumbers kNums = new KNumbers();
@@ -31,6 +47,10 @@ public class KNumbers {
 
 	    result = kNums.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
 	    System.out.println("Top K numbers: " + result);
+	    
+	    int results = kNums.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
+	    System.out.println("Kth smallest number is: " + results);
+
 
 	}
 }
