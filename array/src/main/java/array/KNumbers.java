30481cf658ed18a654c1a463f7dc333ce5e2bc7d
managerwhocodes <managerwhocodes@gmail.com>
2020-09-26 23:06:19 +0530

    Remove K numbers from the array such that there are maximum distinct numbers left

--- array/src/main/java/array/KNumbers.java
+++ array/src/main/java/array/KNumbers.java
@@ -101,6 +101,38 @@ public class KNumbers {
 	    return sortedString.toString();
 	}
 	
+	protected int findMaximumDistinctElements(int[] nums, int k) {
+		int distinctElementsCount = 0;
+	    if (nums.length <= k)
+	    	return distinctElementsCount;
+
+	    Map<Integer, Integer> numFrequencyMap = new HashMap<>();
+	    	for (int i : nums)
+	    		numFrequencyMap.put(i, numFrequencyMap.getOrDefault(i, 0) + 1);
+
+	    PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<Map.Entry<Integer, Integer>>(
+	        (e1, e2) -> e1.getValue() - e2.getValue());
+
+	    for (Map.Entry<Integer, Integer> entry : numFrequencyMap.entrySet()) {
+	    	if (entry.getValue() == 1)
+	    		distinctElementsCount++;
+	    	else
+	    		minHeap.add(entry);
+	    }
+
+	    while (k > 0 && !minHeap.isEmpty()) {
+	    	Map.Entry<Integer, Integer> entry = minHeap.poll();
+	    	k -= entry.getValue() - 1;
+	    	if (k >= 0)
+	    		distinctElementsCount++;
+	    }
+
+	    if (k > 0)
+	      distinctElementsCount -= k;
+
+	    return distinctElementsCount;
+	}
+	
 	public static void main(String[] args) {
 		
 		KNumbers kNums = new KNumbers();
