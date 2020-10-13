df9fabafe5ad55cef8705cac6d0c2fe6f89ad115
managerwhocodes <managerwhocodes@gmail.com>
2020-10-13 23:41:00 +0530

    Given an array of integers between 0 to 9, find two numbers with maximum sum formed by using all digits of the array.

--- array/src/main/java/array/SumArray.java
+++ array/src/main/java/array/SumArray.java
@@ -2,6 +2,7 @@ package array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -122,6 +123,25 @@ public class SumArray {
 		}
 		return false;
 	}
+	
+	protected void maximumSum(Integer[] input) {
+
+		Arrays.sort(input, Comparator.reverseOrder());
+
+		int x = 0;
+		for (int i = 0; i < input.length; i = i + 2) {
+			x = x * 10 + input[i];
+		}
+
+		int y = 0;
+		for (int i = 1; i < input.length; i = i + 2) {
+			y = y * 10 + input[i];
+		}
+
+		System.out.println("The two numbers with maximum sum are "
+									+ x + " and " + y);
+	}
+
 
 
 	public static void main(String[] args) {
