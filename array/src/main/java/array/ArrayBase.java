ae33aa29aed4c171ea770a687139935d4e7c0908
managerwhocodes <managerwhocodes@gmail.com>
2020-10-19 23:25:22 +0530

    Find maximum product of two integers in an array

--- array/src/main/java/array/ArrayBase.java
+++ array/src/main/java/array/ArrayBase.java
@@ -167,6 +167,36 @@ public class ArrayBase {
 		return minDiff;
 	}
 
+	protected void findMaximumProduct(int[] A)
+	{
+
+		int max1 = A[0], max2 = Integer.MIN_VALUE;
+		int min1 = A[0], min2 = Integer.MAX_VALUE;
+
+		for (int i = 1; i < A.length; i++) {
+
+			if (A[i] > max1) {
+				max2 = max1;
+				max1 = A[i];
+			} else if (A[i] > max2) {
+				max2 = A[i];
+			}
+
+			if (A[i] < min1) {
+				min2 = min1;
+				min1 = A[i];
+			} else if (A[i] < min2) {
+				min2 = A[i];
+			}
+		}
+
+		if (max1 * max2 > min1 * min2) {
+			System.out.print("Pair is (" + max1 + ", " + max2 + ")");
+		}
+		else {
+			System.out.print("Pair is (" + min1 + ", " + min2 + ")");
+		}
+	}
 	
 	public static void main(String[] args) {
 		
