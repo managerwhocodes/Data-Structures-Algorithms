a1a6a8c597b812a4b11bf1049657f1afbc66d0d5
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-08 23:59:08 +0530

    Find duplicates in unsorted array

--- array/src/main/java/array/ArrayBase.java
+++ array/src/main/java/array/ArrayBase.java
@@ -336,6 +336,25 @@ public class ArrayBase {
         	return ans; 
     	} 
 	
+	
+	
+	protected Set<Integer> findDuplicates(int[] input) {
+        	Set<Integer> duplicates = new HashSet<Integer>();
+
+        	for (int i = 0; i < input.length; i++) {
+            		for (int j = 1; j < input.length; j++) {
+                		if (input[i] == input[j] && i != j) {
+           				duplicates.add(input[i]);
+                    			break;
+                		}
+            		}
+        	}
+
+        	return duplicates;
+    	}
+
+
+	
 	public static void main(String[] args) {
 		
 		ArrayBase arrayBase = new ArrayBase();
