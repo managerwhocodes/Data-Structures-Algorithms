93be8ab640f5ed5aeae047d9cfb2715d330450e4
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-10 22:58:07 +0530

    Replace every array element by sum of previous and next

--- array/src/main/java/array/ArrayBase.java
+++ array/src/main/java/array/ArrayBase.java
@@ -360,6 +360,24 @@ public class ArrayBase {
 
 	    arr[n - 1] = -1;
 	} 
+	
+	// Replace every array element by sum of previous and next
+	protected void updateArrayBySumOfElements(int arr[], int n) {  
+
+		if (n <= 1) {  
+		    return;  
+		}  
+		int prev = arr[0];  
+		arr[0] = arr[0] + arr[1];  
+  
+
+        	for (int i = 1; i < n - 1; i++) {  
+            		int curr = arr[i];  
+            		arr[i] = prev + arr[i + 1];  
+            		prev = curr;  
+        	}  
+        	arr[n - 1] = prev + arr[n - 1];  
+	}  
 
 	
 	public static void main(String[] args) {
