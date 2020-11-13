1bf79ca1701b85edba6e961023c26aec1e7b891d
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-13 23:45:06 +0530

    Find first occurrence of a number in sorted array

--- array/src/main/java/array/ArrayBase.java
+++ array/src/main/java/array/ArrayBase.java
@@ -401,6 +401,27 @@ public class ArrayBase {
 	    return result;
 	}
 
+	protected int findFirstOccurrence(int[] arr, int x) {
+
+	    int left = 0;
+	    int right = arr.length - 1;
+	    int result = -1;
+
+	    while (left <= right) {
+
+		int mid = (left + right) / 2;
+		    
+		if (x == arr[mid]) {
+		    result = mid;
+		    right = mid - 1;
+		} else if (x < arr[mid]) {
+		    right = mid - 1;
+		} else {
+		    left = mid + 1;
+		}
+	    }
+	    return result;
+	}
 	
 	public static void main(String[] args) {
 		
