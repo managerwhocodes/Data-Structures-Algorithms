b80df6c787180806cae22a417a8fda7991a0fb97
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-22 23:46:24 +0530

    Remove vowels

--- string/src/main/java/string/StringBase.java
+++ string/src/main/java/string/StringBase.java
@@ -170,6 +170,24 @@ public class StringBase {
 		}
 		return minSoFar;
 	}
+	
+	protected String removeVowels(String str) { 
+		
+		 Character vowels[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'}; 
+
+		 List<Character> al = Arrays.asList(vowels); 
+
+		 StringBuffer sb = new StringBuffer(str); 
+
+		 for (int i = 0; i < sb.length(); i++) { 
+
+		     if(al.contains(sb.charAt(i))){ 
+			sb.replace(i, i+1, "") ; 
+			i--; 
+		     } 
+		} 
+		return sb.toString(); 
+    	} 
 
 	public static void main(String[] args) {
 
