babd1a266e3128298b51d6d4dcbe3d6bd383678b
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-26 23:42:34 +0530

    Count common characters in two strings

--- string/src/main/java/string/StringBase.java
+++ string/src/main/java/string/StringBase.java
@@ -227,6 +227,28 @@ public class StringBase {
 		
 		return words.length; 
     	} 
+	
+	protected int countPairs(String s1, int n1, String s2, int n2) { 
+
+	    int []freq1 = new int[26]; 
+	    int []freq2 = new int[26]; 
+
+	    Arrays.fill(freq1, 0); 
+	    Arrays.fill(freq2, 0); 
+
+	    int i, count = 0; 
+
+	    for (i = 0; i < n1; i++) 
+		freq1[s1.charAt(i) - 'a']++; 
+
+	    for (i = 0; i < n2; i++) 
+		freq2[s2.charAt(i) - 'a']++; 
+
+	    for (i = 0; i < 26; i++) 
+		count += (Math.min(freq1[i], freq2[i])); 
+
+	    return count; 
+	} 
 
 	public static void main(String[] args) {
 
