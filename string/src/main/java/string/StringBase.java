4d4803c2afb886fbdbab9b63ff2586791e4efe57
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-11-28 23:40:16 +0530

    Convert characters of a string to opposite case

--- string/src/main/java/string/StringBase.java
+++ string/src/main/java/string/StringBase.java
@@ -249,6 +249,19 @@ public class StringBase {
 
 	    return count; 
 	} 
+	
+	protected void convertCase(StringBuffer str) { 
+        
+		int ln = str.length(); 
+             
+		for (int i=0; i<ln; i++) { 
+		    Character c = str.charAt(i); 
+		    if (Character.isLowerCase(c)) 
+			str.replace(i, i+1, Character.toUpperCase(c)+""); 
+		    else
+			str.replace(i, i+1, Character.toLowerCase(c)+""); 
+		} 
+     	} 
 
 	public static void main(String[] args) {
 
