ba83ceca18ea85dd0a75c8bf508775cec5b4fd1b
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-12-01 23:40:17 +0530

    Reverse words in string

--- string/src/main/java/string/StringReverse.java
+++ string/src/main/java/string/StringReverse.java
@@ -37,6 +37,17 @@ public class StringReverse {
 		reverse(chars, 0, chars.length - 1);
 	}
 	
+	protected String reverseWords(String str) {
+
+	    String words[] = str.split(" ");
+	    String result = "";
+
+	    for (int i = words.length - 1; i >= 0; i--) { 
+		result = result + words[i] + " "; 
+	    }
+	    return result;
+	}
+	
 	
 	public static void main(String[] args) {
 		
