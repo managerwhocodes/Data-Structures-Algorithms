01242c9e3b38c97e40c46ed8f776653698bd2191
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2020-12-18 23:37:34 +0530

    Calculate the frequency of each word in the given string

--- string/src/main/java/string/StringBase.java
+++ string/src/main/java/string/StringBase.java
@@ -475,6 +475,25 @@ public class StringBase {
 	    } 
 	    return '\0'; 
 	} 
+	
+	protected void countFrequencyOfWords(String str) {
+
+	    Map<String,Integer> map = new TreeMap<>();
+
+	    String arr[]=str.split(" ");
+
+	    for(int i=0;i<arr.length;i++) {
+
+		if(map.containsKey(arr[i]))
+		    map.put(arr[i], map.get(arr[i])+1);
+		else
+		    map.put(arr[i],1);  
+	    }
+
+	    for(Map.Entry<String,Integer> entry : map.entrySet()) {
+		System.out.println(entry.getKey() + " - " + entry.getValue());
+	    }
+	}
 
 	public static void main(String[] args) {
 
