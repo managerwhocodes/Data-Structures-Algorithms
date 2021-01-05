af8c051611ab00257a720ee9fc495619a1a688f9
managerwhocodes <62826378+managerwhocodes@users.noreply.github.com>
2021-01-05 23:58:58 +0530

    Check if two strings can become same after a swap in one string

--- string/src/main/java/string/StringBase.java
+++ string/src/main/java/string/StringBase.java
@@ -686,6 +686,35 @@ public class StringBase {
 	    } 
 	    return count; 
 	} 
+	
+	protected boolean areMetaStrings(String str1, String str2) { 
+    
+	    int len1 = str1.length(); 
+	    int len2 = str2.length(); 
+
+	    if (len1 != len2) 
+		return false; 
+
+	    int prev = -1, curr = -1; 
+	    int count = 0; 
+
+	    for (int i=0; i<len1; i++) { 
+
+		if (str1.charAt(i) != str2.charAt(i)) { 
+
+		    count++; 
+
+		    if (count > 2) 
+			return false; 
+
+		    prev = curr; 
+		    curr = i; 
+		} 
+	    } 
+	    return (count == 2 && 
+		    str1.charAt(prev) == str2.charAt(curr) && 
+		    str1.charAt(curr) == str2.charAt(prev)); 
+	} 
 
 	public static void main(String[] args) {
 
